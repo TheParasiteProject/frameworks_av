07789033dcbd9a86efde5d56920a766f66105e79
Wonsik Kim <wonsik@google.com>
2025-03-11 17:17:18 -0700

    ApexCodecs: add SupportedValues, SettingsResult, and ParamDescriptor
    Bug: 401332082
    Test: atest ApexCodecsTest
    Flag: android.media.swcodec.flags.apexcodecs_base
    Change-Id: Ib9b59f3f5613923845aa278155ef122edb4605d5

--- media/module/libapexcodecs/ApexCodecs.cpp
+++ media/module/libapexcodecs/ApexCodecs.cpp
@@ -19,7 +19,10 @@
 #include <android-base/logging.h>
 
 #include <new>
+#include <map>
+#include <vector>
 
+#include <C2ParamInternal.h>
 #include <android_media_swcodec_flags.h>
 
 #include <android-base/no_destructor.h>
@@ -386,16 +389,240 @@ ApexCodec_Status ApexCodec_Buffer_getConfigUpdates(
     return buffer->getConfigUpdates(outConfigUpdates, outOwnedByClient);
 }
 
+struct ApexCodec_SupportedValues {
+public:
+    ApexCodec_SupportedValues(
+            const C2FieldSupportedValues &supportedValues,
+            const C2Value::type_t &numberType) {
+        mType = (ApexCodec_SupportedValuesType)supportedValues.type;
+        mNumberType = (ApexCodec_SupportedValuesNumberType)numberType;
+        switch (supportedValues.type) {
+            case C2FieldSupportedValues::RANGE: {
+                mValues.insert(mValues.end(), 5, ApexCodec_Value{});
+                ToApexCodecValue(supportedValues.range.min,   numberType, &mValues[0]);
+                ToApexCodecValue(supportedValues.range.max,   numberType, &mValues[1]);
+                ToApexCodecValue(supportedValues.range.step,  numberType, &mValues[2]);
+                ToApexCodecValue(supportedValues.range.num,   numberType, &mValues[3]);
+                ToApexCodecValue(supportedValues.range.denom, numberType, &mValues[4]);
+                break;
+            }
+            case C2FieldSupportedValues::VALUES:
+            case C2FieldSupportedValues::FLAGS: {
+                for (size_t i = 0; i < supportedValues.values.size(); ++i) {
+                    mValues.emplace_back();
+                    ToApexCodecValue(supportedValues.values[i], numberType, &mValues[i]);
+                }
+                break;
+            }
+            default:
+                // Unrecognized type; initialize as empty.
+                mType = APEXCODEC_SUPPORTED_VALUES_EMPTY;
+                break;
+        }
+    }
+
+    ~ApexCodec_SupportedValues() {
+    }
+
+    ApexCodec_Status getTypeAndValues(
+            ApexCodec_SupportedValuesType *type,
+            ApexCodec_SupportedValuesNumberType *numberType,
+            ApexCodec_Value **values,
+            uint32_t *numValues) {
+        if (type == nullptr) {
+            return APEXCODEC_STATUS_BAD_VALUE;
+        }
+        if (numberType == nullptr) {
+            return APEXCODEC_STATUS_BAD_VALUE;
+        }
+        if (values == nullptr) {
+            return APEXCODEC_STATUS_BAD_VALUE;
+        }
+        if (numValues == nullptr) {
+            return APEXCODEC_STATUS_BAD_VALUE;
+        }
+        *type = mType;
+        *numberType = mNumberType;
+        switch (mType) {
+            case APEXCODEC_SUPPORTED_VALUES_EMPTY: {
+                *values = nullptr;
+                *numValues = 0;
+                break;
+            }
+            case APEXCODEC_SUPPORTED_VALUES_RANGE:
+            case APEXCODEC_SUPPORTED_VALUES_VALUES:
+            case APEXCODEC_SUPPORTED_VALUES_FLAGS: {
+                if (mValues.empty()) {
+                    return APEXCODEC_STATUS_BAD_STATE;
+                }
+                *values = mValues.data();
+                *numValues = mValues.size();
+                break;
+            }
+            default:
+                return APEXCODEC_STATUS_BAD_STATE;
+        }
+        return APEXCODEC_STATUS_OK;
+    }
+
+    static bool ToApexCodecValue(
+            const C2Value::Primitive &value,
+            const C2Value::type_t &type,
+            ApexCodec_Value *outValue) {
+        switch (type) {
+            case C2Value::NO_INIT:
+                return false;
+            case C2Value::INT32:
+                outValue->i32 = value.i32;
+                return true;
+            case C2Value::UINT32:
+                outValue->u32 = value.u32;
+                return true;
+            case C2Value::INT64:
+                outValue->i64 = value.i64;
+                return true;
+            case C2Value::UINT64:
+                outValue->u64 = value.u64;
+                return true;
+            case C2Value::FLOAT:
+                outValue->f = value.fp;
+                return true;
+            default:
+                return false;
+        }
+    }
+
+    static C2Value::type_t GetFieldType(
+            const std::shared_ptr<C2ParamReflector> &reflector,
+            const C2ParamField& field) {
+        std::unique_ptr<C2StructDescriptor> desc = reflector->describe(
+                _C2ParamInspector::GetIndex(field));
+
+        for (const C2FieldDescriptor &fieldDesc : *desc) {
+            if (_C2ParamInspector::GetOffset(fieldDesc) == _C2ParamInspector::GetOffset(field)) {
+                if (_C2ParamInspector::GetSize(fieldDesc) != _C2ParamInspector::GetSize(field)) {
+                    // Size doesn't match.
+                    return C2Value::NO_INIT;
+                }
+                switch (fieldDesc.type()) {
+                    case C2FieldDescriptor::INT32:
+                    case C2FieldDescriptor::UINT32:
+                    case C2FieldDescriptor::INT64:
+                    case C2FieldDescriptor::UINT64:
+                    case C2FieldDescriptor::FLOAT:
+                        return (C2Value::type_t)fieldDesc.type();
+                    default:
+                        // Unrecognized type.
+                        return C2Value::NO_INIT;
+                }
+            }
+        }
+        return C2Value::NO_INIT;
+    }
+
+private:
+    ApexCodec_SupportedValuesType mType;
+    ApexCodec_SupportedValuesNumberType mNumberType;
+    std::vector<ApexCodec_Value> mValues;
+};
+
 ApexCodec_Status ApexCodec_SupportedValues_getTypeAndValues(
         ApexCodec_SupportedValues *supportedValues,
         ApexCodec_SupportedValuesType *type,
         ApexCodec_SupportedValuesNumberType *numberType,
         ApexCodec_Value **values,
         uint32_t *numValues) {
-    return APEXCODEC_STATUS_OMITTED;
+    if (supportedValues == nullptr) {
+        return APEXCODEC_STATUS_BAD_VALUE;
+    }
+    return supportedValues->getTypeAndValues(type, numberType, values, numValues);
 }
 
-void ApexCodec_SupportedValues_destroy(ApexCodec_SupportedValues *values) {}
+void ApexCodec_SupportedValues_destroy(ApexCodec_SupportedValues *values) {
+    delete values;
+}
+
+struct ApexCodec_SettingResults {
+public:
+    explicit ApexCodec_SettingResults(
+            const std::shared_ptr<C2ParamReflector> &reflector,
+            const std::vector<C2SettingResult> &results) : mReflector(reflector) {
+        for (const C2SettingResult &c2Result : results) {
+            mResults.emplace_back();
+            Entry &entry = mResults.back();
+            entry.failure = (ApexCodec_SettingResultFailure)c2Result.failure;
+            entry.field.index = _C2ParamInspector::GetIndex(c2Result.field.paramOrField);
+            entry.field.offset = _C2ParamInspector::GetOffset(c2Result.field.paramOrField);
+            entry.field.size = _C2ParamInspector::GetSize(c2Result.field.paramOrField);
+            if (c2Result.field.values) {
+                entry.fieldValues = std::make_unique<ApexCodec_SupportedValues>(
+                        *c2Result.field.values,
+                        ApexCodec_SupportedValues::GetFieldType(mReflector,
+                                                                c2Result.field.paramOrField));
+                entry.field.values = entry.fieldValues.get();
+            } else {
+                entry.field.values = nullptr;
+            }
+            for (const C2ParamFieldValues &c2Conflict : c2Result.conflicts) {
+                entry.conflicts.emplace_back();
+                ApexCodec_ParamFieldValues &conflict = entry.conflicts.back();
+                conflict.index = _C2ParamInspector::GetIndex(c2Conflict.paramOrField);
+                conflict.offset = _C2ParamInspector::GetOffset(c2Conflict.paramOrField);
+                conflict.size = _C2ParamInspector::GetSize(c2Conflict.paramOrField);
+                if (c2Conflict.values) {
+                    entry.conflictValues.emplace_back(std::make_unique<ApexCodec_SupportedValues>(
+                            *c2Conflict.values,
+                            ApexCodec_SupportedValues::GetFieldType(mReflector,
+                                                                    c2Conflict.paramOrField)));
+                    conflict.values = entry.conflictValues.back().get();
+                } else {
+                    conflict.values = nullptr;
+                }
+            }
+        }
+    }
+
+    ~ApexCodec_SettingResults() {
+    }
+
+    ApexCodec_Status getResultAtIndex(
+            size_t index,
+            ApexCodec_SettingResultFailure *failure,
+            ApexCodec_ParamFieldValues *field,
+            ApexCodec_ParamFieldValues **conflicts,
+            size_t *numConflicts) {
+        if (failure == nullptr) {
+            return APEXCODEC_STATUS_BAD_VALUE;
+        }
+        if (field == nullptr) {
+            return APEXCODEC_STATUS_BAD_VALUE;
+        }
+        if (conflicts == nullptr) {
+            return APEXCODEC_STATUS_BAD_VALUE;
+        }
+        if (numConflicts == nullptr) {
+            return APEXCODEC_STATUS_BAD_VALUE;
+        }
+        if (index >= mResults.size()) {
+            return APEXCODEC_STATUS_NOT_FOUND;
+        }
+        *failure = mResults[index].failure;
+        *field = mResults[index].field;
+        *conflicts = mResults[index].conflicts.data();
+        *numConflicts = mResults[index].conflicts.size();
+        return APEXCODEC_STATUS_OK;
+    }
+private:
+    std::shared_ptr<C2ParamReflector> mReflector;
+    struct Entry {
+        ApexCodec_SettingResultFailure failure;
+        ApexCodec_ParamFieldValues field;
+        std::vector<ApexCodec_ParamFieldValues> conflicts;
+        std::unique_ptr<ApexCodec_SupportedValues> fieldValues;
+        std::vector<std::unique_ptr<ApexCodec_SupportedValues>> conflictValues;
+    };
+    std::vector<Entry> mResults;
+};
 
 ApexCodec_Status ApexCodec_SettingResults_getResultAtIndex(
         ApexCodec_SettingResults *results,
@@ -404,10 +631,15 @@ ApexCodec_Status ApexCodec_SettingResults_getResultAtIndex(
         ApexCodec_ParamFieldValues *field,
         ApexCodec_ParamFieldValues **conflicts,
         size_t *numConflicts) {
-    return APEXCODEC_STATUS_OMITTED;
+    if (results == nullptr) {
+        return APEXCODEC_STATUS_BAD_VALUE;
+    }
+    return results->getResultAtIndex(index, failure, field, conflicts, numConflicts);
 }
 
-void ApexCodec_SettingResults_destroy(ApexCodec_SettingResults *results) {}
+void ApexCodec_SettingResults_destroy(ApexCodec_SettingResults *results) {
+    delete results;
+}
 
 ApexCodec_Status ApexCodec_Component_process(
         ApexCodec_Component *comp,
@@ -434,11 +666,90 @@ ApexCodec_Status ApexCodec_Configurable_query(
     return APEXCODEC_STATUS_OMITTED;
 }
 
+struct ApexCodec_ParamDescriptors {
+public:
+    explicit ApexCodec_ParamDescriptors(
+            const std::vector<std::shared_ptr<C2ParamDescriptor>> &paramDescriptors) {
+        for (const std::shared_ptr<C2ParamDescriptor> &c2Descriptor : paramDescriptors) {
+            if (!c2Descriptor) {
+                continue;
+            }
+            uint32_t index = c2Descriptor->index();
+            Entry &entry = mDescriptors[index];
+            entry.index = index;
+            entry.attr = (ApexCodec_ParamAttribute)_C2ParamInspector::GetAttrib(*c2Descriptor);
+            entry.name = c2Descriptor->name();
+            for (const C2Param::Index &dependency : c2Descriptor->dependencies()) {
+                entry.dependencies.emplace_back((uint32_t)dependency);
+            }
+            mIndices.push_back(entry.index);
+        }
+    }
+
+    ~ApexCodec_ParamDescriptors() {
+    }
+
+    ApexCodec_Status getIndices(uint32_t **indices, size_t *numIndices) {
+        if (indices == nullptr) {
+            return APEXCODEC_STATUS_BAD_VALUE;
+        }
+        if (numIndices == nullptr) {
+            return APEXCODEC_STATUS_BAD_VALUE;
+        }
+        *indices = mIndices.data();
+        *numIndices = mIndices.size();
+        return APEXCODEC_STATUS_OK;
+    }
+
+    ApexCodec_Status getDescriptor(
+            uint32_t index,
+            ApexCodec_ParamAttribute *attr,
+            const char **name,
+            uint32_t **dependencies,
+            size_t *numDependencies) {
+        if (attr == nullptr) {
+            return APEXCODEC_STATUS_BAD_VALUE;
+        }
+        if (name == nullptr) {
+            return APEXCODEC_STATUS_BAD_VALUE;
+        }
+        if (dependencies == nullptr) {
+            return APEXCODEC_STATUS_BAD_VALUE;
+        }
+        if (numDependencies == nullptr) {
+            return APEXCODEC_STATUS_BAD_VALUE;
+        }
+        auto it = mDescriptors.find(index);
+        if (it == mDescriptors.end()) {
+            return APEXCODEC_STATUS_BAD_VALUE;
+        }
+        const Entry &entry = it->second;
+        *attr = entry.attr;
+        *name = entry.name.c_str();
+        *dependencies = const_cast<uint32_t *>(entry.dependencies.data());
+        *numDependencies = entry.dependencies.size();
+        return APEXCODEC_STATUS_OK;
+    }
+
+private:
+    struct Entry {
+        uint32_t index;
+        ApexCodec_ParamAttribute attr;
+        C2String name;
+        std::vector<uint32_t> dependencies;
+    };
+    std::map<uint32_t, Entry> mDescriptors;
+    std::vector<uint32_t> mIndices;
+};
+
 ApexCodec_Status ApexCodec_ParamDescriptors_getIndices(
         ApexCodec_ParamDescriptors *descriptors,
         uint32_t **indices,
         size_t *numIndices) {
-    return APEXCODEC_STATUS_OMITTED;
+    if (descriptors == nullptr) {
+        return APEXCODEC_STATUS_BAD_VALUE;
+    }
+    return descriptors->getIndices(indices, numIndices);
 }
 
 ApexCodec_Status ApexCodec_ParamDescriptors_getDescriptor(
@@ -448,10 +759,14 @@ ApexCodec_Status ApexCodec_ParamDescriptors_getDescriptor(
         const char **name,
         uint32_t **dependencies,
         size_t *numDependencies) {
-    return APEXCODEC_STATUS_OMITTED;
+    if (descriptors == nullptr) {
+        return APEXCODEC_STATUS_BAD_VALUE;
+    }
+    return descriptors->getDescriptor(index, attr, name, dependencies, numDependencies);
 }
 
 void ApexCodec_ParamDescriptors_destroy(ApexCodec_ParamDescriptors *descriptors) {
+    delete descriptors;
 }
 
 ApexCodec_Status ApexCodec_Configurable_querySupportedParams(
