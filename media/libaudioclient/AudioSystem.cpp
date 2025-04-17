faa9336a3f7b705f660d82a039869b44c1671bc8
Andy Hung <hunga@google.com>
2025-04-16 17:50:12 -0700

    Revert "AudioSystem: Ensure onNewService callback is complete before returning service"
    This reverts commit 9fb6f3dcf102b95440a6d519eb974977c4fe45cb.
    
    No longer needed because of commit
    ccbb7422419027795096cd7d3b14fba963e4e8a7
    ServiceSingleton: Stricter onNewService callback handling
    
    Flag: EXEMPT bugfix
    Test: atest WvtsDeviceTestCases:com.google.android.wvts.MediaDrmParameterizedTests#testResourceRatingTierNumKeys[L1] --iterations=100 -- --abi arm64-v8a
    Bug: 403676413
    Change-Id: Ic4b49e9176ab27d54459a359f203c1517723124d

--- media/libaudioclient/AudioSystem.cpp
+++ media/libaudioclient/AudioSystem.cpp
@@ -197,7 +197,6 @@ public:
         }
         if (mValid) return mService;
         if (waitMs.count() < 0) waitMs = mWaitMs;
-        auto timepointLimit = std::chrono::steady_clock::now() + waitMs;
         ul.unlock();
 
         // mediautils::getService() installs a persistent new service notification.
@@ -208,7 +207,6 @@ public:
         ul.lock();
         // return the IAudioFlinger interface which is adapted
         // from the media::IAudioFlingerService.
-        mCv.wait_until(ul, timepointLimit, isServiceValid_l);
         return mService;
     }
 
@@ -293,7 +291,6 @@ private:
             mService = service;
             client = mClient;
             mValid = true;
-            mCv.notify_all();
         }
         // TODO(b/375280520) consider registerClient() within mMutex lock.
         const int64_t token = IPCThreadState::self()->clearCallingIdentity();
@@ -308,12 +305,7 @@ private:
         return sp<AudioFlingerClientAdapter>::make(af);
     }
 
-    static bool isServiceValid_l() REQUIRES(mMutex) {
-        return mValid;
-    }
-
     static inline constinit std::mutex mMutex;
-    static inline constinit std::condition_variable mCv;
     static inline constinit sp<AudioSystem::AudioFlingerClient> mClient GUARDED_BY(mMutex);
     static inline constinit sp<IAudioFlinger> mService GUARDED_BY(mMutex);
     static inline constinit std::chrono::milliseconds mWaitMs
@@ -1032,7 +1024,6 @@ public:
             client = mClient;
             mService = aps;
             mValid = true;
-            mCv.notify_all();
         }
         // TODO(b/375280520) consider registerClient() within mMutex lock.
         const int64_t token = IPCThreadState::self()->clearCallingIdentity();
@@ -1093,7 +1084,6 @@ public:
         }
         if (mValid) return mService;
         if (waitMs.count() < 0) waitMs = mWaitMs;
-        auto timepointLimit = std::chrono::steady_clock::now() + waitMs;
         ul.unlock();
 
         auto service = mediautils::getService<
@@ -1104,7 +1094,6 @@ public:
         // (whereupon mService contained the actual local service pointer to use).
         // we should always return mService.
         ul.lock();
-        mCv.wait_until(ul, timepointLimit, isServiceValid_l);
         return mService;
     }
 
@@ -1153,12 +1142,7 @@ public:
     }
 private:
 
-    static bool isServiceValid_l() REQUIRES(mMutex) {
-        return mValid;
-    }
-
     static inline constinit std::mutex mMutex;
-    static inline constinit std::condition_variable mCv;
     static inline constinit sp<AudioSystem::AudioPolicyServiceClient> mClient GUARDED_BY(mMutex);
     static inline constinit sp<IAudioPolicyService> mService GUARDED_BY(mMutex);
     static inline constinit bool mValid GUARDED_BY(mMutex) = false;
