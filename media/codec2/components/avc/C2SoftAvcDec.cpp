83e4ef3f0a09059b13047771a67772eb2290fcfc
Ram Mohan M <rammo@google.com>
2025-06-17 22:25:22 +0000

    C2SoftVideoDec: Initialize result field in C2Work
    final outcome of the work is not initialized during cloneAndSend. This
    is now set to C2_OK.
    
    Also call cloneAndSend for configUpdate only if it is not already done.
    
    Bug: 425444898
    Bug: 414353388
    Test: atest MctsMediaV2TestCases
    Test: atest MctsMediaDecoderTestCases
    Test: atest VtsHalMediaC2V1_0TargetVideoDecTest
    Flag: EXEMPT bugfix
    Change-Id: I2b6bef324abbd7bebdfa65d877e324e85df9f1a3

--- media/codec2/components/avc/C2SoftAvcDec.cpp
+++ media/codec2/components/avc/C2SoftAvcDec.cpp
@@ -841,6 +841,7 @@ void C2SoftAvcDec::process(
     }
     bool eos = ((work->input.flags & C2FrameData::FLAG_END_OF_STREAM) != 0);
     bool hasPicture = false;
+    bool configUpdateQueued = false;
 
     ALOGV("in buffer attr. size %zu timestamp %d frameindex %d, flags %x",
           inSize, (int)work->input.ordinal.timestamp.peeku(),
@@ -967,6 +968,7 @@ void C2SoftAvcDec::process(
         hasPicture |= (1 == ps_decode_op->u4_frame_decoded_flag);
         if (ps_decode_op->u4_output_present) {
             finishWork(ps_decode_op->u4_ts, work);
+            configUpdateQueued = c2_cntr64_t(ps_decode_op->u4_ts) == work->input.ordinal.frameIndex;
         }
         inPos += ps_decode_op->u4_num_bytes_consumed;
     }
@@ -975,14 +977,15 @@ void C2SoftAvcDec::process(
         mSignalledOutputEos = true;
     } else if (!hasPicture) {
         fillEmptyWork(work);
-    } else if (work->worklets.front()->output.configUpdate.size() > 0) {
+    } else if (!configUpdateQueued && work->worklets.front()->output.configUpdate.size() > 0) {
         auto fillWork = [&work](const std::unique_ptr<C2Work>& cloneWork) {
-            cloneWork->worklets.front()->output.flags = (C2FrameData::flags_t)0;
+            cloneWork->worklets.front()->output.flags = C2FrameData::FLAG_INCOMPLETE;
             cloneWork->worklets.front()->output.buffers.clear();
             cloneWork->worklets.front()->output.configUpdate =
                     std::move(work->worklets.front()->output.configUpdate);
             cloneWork->worklets.front()->output.ordinal = work->input.ordinal;
             cloneWork->workletsProcessed = 1u;
+            cloneWork->result = C2_OK;
         };
         uint64_t frameIndex = work->input.ordinal.frameIndex.peeku();
         cloneAndSend(frameIndex, work, fillWork);
