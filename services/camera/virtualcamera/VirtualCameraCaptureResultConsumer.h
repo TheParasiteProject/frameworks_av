1b8b80d8d5edcb0e7e59f6f548fead17a1182c23
Dorin Drimus <dorindrimus@google.com>
2025-07-11 06:38:45 -0700

    Merge CaptureResult metadata in the VirtualCameraService
    Bug: 371167033
    Test: atest VirtualCameraTest
    Test: atest virtual_camera_tests
    Flag: android.companion.virtualdevice.flags.virtual_camera_metadata
    Change-Id: Ibe76292429f85cbdc45a79c5190ce4892d6b3b81

--- services/camera/virtualcamera/VirtualCameraCaptureResultConsumer.h
+++ services/camera/virtualcamera/VirtualCameraCaptureResultConsumer.h
@@ -14,38 +14,42 @@
  * limitations under the License.
  */
 
- #ifndef ANDROID_COMPANION_VIRTUALCAMERA_VIRTUALCAMERACAPTURERESULTCONSUMER_H
- #define ANDROID_COMPANION_VIRTUALCAMERA_VIRTUALCAMERACAPTURERESULTCONSUMER_H
+#ifndef ANDROID_COMPANION_VIRTUALCAMERA_VIRTUALCAMERACAPTURERESULTCONSUMER_H
+#define ANDROID_COMPANION_VIRTUALCAMERA_VIRTUALCAMERACAPTURERESULTCONSUMER_H
 
 #include <mutex>
 
-#include "aidl/android/hardware/camera/device/CameraMetadata.h"
 #include "aidl/android/companion/virtualcamera/BnCaptureResultConsumer.h"
 #include "android-base/thread_annotations.h"
+#include "system/camera_metadata.h"
 
- namespace android {
- namespace companion {
- namespace virtualcamera {
+namespace android {
+namespace companion {
+namespace virtualcamera {
 
- // Consumer for capture results, filled by the client application and consumed
- // by the VirtualCameraRenderThread.
- class VirtualCameraCaptureResultConsumer
-     : public aidl::android::companion::virtualcamera::BnCaptureResultConsumer {
-  public:
-   ndk::ScopedAStatus acceptCaptureResult(
-       int64_t timestamp,
-       const aidl::android::companion::virtualcamera::VirtualCameraMetadata&
-           captureResultMetadata);
+// Consumer for capture results, filled by the client application and consumed
+// by the VirtualCameraRenderThread.
+class VirtualCameraCaptureResultConsumer
+    : public aidl::android::companion::virtualcamera::BnCaptureResultConsumer {
+ public:
+  ~VirtualCameraCaptureResultConsumer();
 
-  private:
-    std::mutex mLock;
-    int64_t mLastTimestamp GUARDED_BY(mLock);
-    std::unique_ptr<aidl::android::hardware::camera::device::CameraMetadata>
-        mLastMetadata GUARDED_BY(mLock);
- };
+  ndk::ScopedAStatus acceptCaptureResult(
+      int64_t timestamp,
+      const aidl::android::companion::virtualcamera::VirtualCameraMetadata&
+          captureResultMetadata) override;
 
- }  // namespace virtualcamera
- }  // namespace companion
- }  // namespace android
+  const camera_metadata_t* getCaptureResultMetadataForTimestamp(
+      int64_t timestamp);
 
- #endif  // ANDROID_COMPANION_VIRTUALCAMERA_VIRTUALCAMERACAPTURERESULTCONSUMER_H
+ private:
+  std::mutex mLock;
+  int64_t mLastTimestamp GUARDED_BY(mLock);
+  camera_metadata_t* mLastMetadata GUARDED_BY(mLock) = nullptr;
+};
+
+}  // namespace virtualcamera
+}  // namespace companion
+}  // namespace android
+
+#endif  // ANDROID_COMPANION_VIRTUALCAMERA_VIRTUALCAMERACAPTURERESULTCONSUMER_H
